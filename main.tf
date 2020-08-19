8a6a5be2856f5fd72ed8cf02e3ed65a1a4c32e60
Mayank Gaur <37980122+mykg@users.noreply.github.com>
2020-08-19 21:15:25 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,154 @@
+# configure the provider
+provider "aws" {
+  region = "ap-south-1"
+  profile = "new_tf"
+}
+
+# creating a vpc
+resource "aws_vpc" "vpc" {
+  cidr_block       = "192.169.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+# creating subnet in 1a
+resource "aws_subnet" "sub_1a" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "192.169.1.0/24"
+  availability_zone = "ap-south-1a" 
+
+  tags = {
+    Name = "sub_1a"
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
+
+# creating a subnet in 1b
+resource "aws_subnet" "sub_1b" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "191.169.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "sub_1b"
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
+
+# creating igw
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name = "igw"
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
+
+# creating route table
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags = {
+    Name = "r"
+  }
+  depends_on = [ aws_internet_gateway.igw ]
+}
+
+# associating route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.sub_1a.id
+  route_table_id = aws_route_table.r.id
+}
+
+# sg for wordpress
+resource "aws_security_group" "wp_sg" {
+  name        = "wp sg"
+  description = "Allow http ssh all"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wp sg"
+  }
+}
+
+# sg for mysql
+resource "aws_security_group" "mysql_sg" {
+  name        = "mysql sg"
+  description = ""
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    description = "mysql port"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+  
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysql sg"
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
